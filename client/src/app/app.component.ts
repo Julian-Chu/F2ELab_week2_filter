ebfecbeb9479be573f5b7c4eb7b2f32b3e425b38
JulianChu <yulang.chu@gmail.com>
2018-06-17 22:04:25 +0200

    implement search

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from "@angular/core";
+// tslint:disable:use-life-cycle-interface
+// tslint:disable:quotemark
+import { Component, ViewChild } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { NgModel } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -9,6 +12,10 @@ import { HttpClient } from "@angular/common/http";
 export class AppComponent {
   title = "app";
   data: any;
+  events: any = [];
+  keyword = "";
+
+  @ViewChild("tKeyword") tKeyword: NgModel;
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.http
@@ -16,7 +23,26 @@ export class AppComponent {
         "https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97"
       )
       .subscribe((value: any) => {
-        this.data = value.result.records.slice(0, 3);
+        this.events = value.result.records.slice(0, 10);
+        this.data = this.events;
       });
+  }
+
+  ngAfterViewInit() {
+    this.tKeyword.valueChanges.subscribe(key => {
+      if (key === "" || key === null || key === undefined) {
+        this.data = this.events;
+      } else {
+        this.data = this.events.filter(event => {
+          return (
+            event.Description.includes(key) ||
+            event.Add.includes(key) ||
+            event.Name.includes(key)
+          );
+        });
+        console.log(this.data);
+        console.log(this.events);
+      }
+    });
   }
 }
